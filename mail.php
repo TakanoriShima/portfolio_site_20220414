887d5605089fefe16b2f954d6bfd144809636ce4
TakanoriShima <quark2galaxy@gmail.com>
2022-04-14 12:47:57 +0900

    修正

---
+++ mail.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Ajax通信で飛んできた値を取得
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    // メールを送信するコードはご自身でお書きください。
+    // 参考 https://naoya-ono.com/blog/ajax-send-mail/
